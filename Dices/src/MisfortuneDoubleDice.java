b14efb5f3aae8410ab8da3a6465fc201b3c97262
Ismael Sousa <165102646+ISMAELSOUSAFONTELESCASTRO@users.noreply.github.com>
2024-07-10 23:04:11 -0300

    Comment MisfortuneDoubleDice.java

--- Dices/src/MisfortuneDoubleDice.java
+++ Dices/src/MisfortuneDoubleDice.java
@@ -1,9 +1,10 @@
 public class MisfortuneDoubleDice extends DoubleDice {
+    /*Iniciando esta classe filha*/
     public MisfortuneDoubleDice(){
         super();
     }
 
     @Override public int ThrowDices(){
-        return rdm.nextInt(3) + rdm.nextInt(3) + 2;
+        return rdm.nextInt(3) + rdm.nextInt(3) + 2;//Dado Azarado, range de 2 à 6
     }
 }
