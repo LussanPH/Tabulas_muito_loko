d30a77f4cd4c6daa02aee4053c449ac66c5f1289
Ismael Sousa <165102646+ISMAELSOUSAFONTELESCASTRO@users.noreply.github.com>
2024-07-10 23:02:18 -0300

    Comment LuckyDoubleDice.java

--- Dices/src/LuckyDoubleDice.java
+++ Dices/src/LuckyDoubleDice.java
@@ -1,9 +1,10 @@
 public class LuckyDoubleDice extends DoubleDice {
+    /*Iniciando esta classe filha*/
     public LuckyDoubleDice(){
         super();
     }
-
+    
     @Override public int ThrowDices(){
-        return rdm.nextInt(4) + rdm.nextInt(3) + 7;
+        return rdm.nextInt(4) + rdm.nextInt(3) + 7;//Dados sortudos, vão de 7 à 12
     }
 }
