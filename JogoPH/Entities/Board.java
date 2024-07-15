c5ba6fb41abf96918408704228beb323ee69a78c
LussanPH <166248501+LussanPH@users.noreply.github.com>
2024-07-15 00:30:26 -0300

    Update Board.java
    Melhorada no board

--- JogoPH/Entities/Board.java
+++ JogoPH/Entities/Board.java
@@ -1,18 +1,31 @@
-package Entities;
+import java.util.Scanner;
+import Entities.Board;
+import Entities.Persons;
 import java.util.List;
 import java.util.ArrayList;
-public class Board {
-    private List<String>[] board;
-    @SuppressWarnings("unchecked")
-    public Board(){
-        board = new ArrayList[40];
+public class Principal{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Board b = new Board();
+        List<String> cores = new ArrayList<>();
+        cores.add("vermelho");
+        cores.add("amarelo");
+        cores.add("azul");
+        cores.add("preto");
+        cores.add("branco");
+        cores.add("marrom");
+        System.out.print("Bem vindo ao JUJUTSU NO CASA BATTLE ROYALE" + "\n digite quantos jogadores participarão: ");
+        int numero = sc.nextInt();
+        Persons[] p = new Persons[numero]; /**Vetor com cada elemento sendo as pessoas que estão no jogo */
+        sc.nextLine();
+        for(int i = 0; i<numero; i++){
+            System.out.print("Jogador " + 0 + (i+1) + " digite uma das cores disponíveis");/**Seleção das cores */
+            System.out.print(cores + ": ");
+            String c = sc.nextLine();
+            p[i].setCor(c);
+            cores.remove(c);
+        }
+        
+        sc.close();
     }
-
-    public void addPerson(String cor, int casa){
-        board[casa].add(cor);
-    }
-
-    public void removePerson(String cor, int casa){
-        board[casa].remove(cor);
-    }
-}
+}   
