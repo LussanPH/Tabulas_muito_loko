9b3c9fad834c729f03be4950e54449f048a49af9
LussanPH <166248501+LussanPH@users.noreply.github.com>
2024-07-14 23:18:36 -0300

    Add files via upload

---
+++ JogoPH/Entities/Azarado.java
@@ -0,0 +1,8 @@
+package Entities;
+
+public class Azarado extends Persons{
+    public Azarado(String cor, int casa, int nJogada, int status){
+        super(cor, nJogada, casa, status);
+    }
+    @Override
+}
